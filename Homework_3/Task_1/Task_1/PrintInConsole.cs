50c19d09f76061fcd49b85dde774fdf45dbaeb56
viksamiles77 <mileskiviktor77@gmail.com>
2024-04-02 13:01:03 +0200

    Task 1 done

---
+++ Homework_3/Task_1/Task_1/PrintInConsole.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_1
+{
+    public class PrintInConsole
+    {
+        public void Print<T>(T item)
+        {
+            Console.WriteLine(item);
+        }
+
+        public void PrintCollection<T>(List<T> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Homework_3/Task_1/Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintInConsole printer = new PrintInConsole();
+            printer.Print("Hello, World!");
+
+            List<string> strings = new List<string> { "Good Morning!", "Good Evening!", "Good Night!" };
+            printer.PrintCollection(strings);
+
+            List<int> ints = new List<int> { 1, 2, 3, 4, 5 };
+            printer.PrintCollection(ints);
+        }
+    }
+}
