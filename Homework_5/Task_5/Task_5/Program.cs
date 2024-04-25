1f968749b9a0fc3e7aca44fe9ac6bcb21371a1b7
viksamiles77 <mileskiviktor77@gmail.com>
2024-04-25 16:42:57 +0200

    Task 5

---
+++ Homework_5/Task_5/Task_5/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = "Exercise";
+            string filePath = @$"{folderPath}\calculations.txt";
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            using (StreamWriter file = new StreamWriter(filePath, true))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write("Enter the first number: ");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter the second number: ");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+
+                    string result = Calculate(num1, num2);
+
+                    Console.WriteLine($"Result: {result}");
+
+                    string timeStamp = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+                    file.WriteLine($"{timeStamp} --> {result}");
+                }
+            }
+        }
+
+        static string Calculate(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return $"{num1} + {num2} = {result}";
+        }
+    }
+}
